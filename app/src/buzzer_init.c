b365dc9f8171f46db2cf6e564f88aa7699f2b389
secretangel77 <67867308+secretangel77@users.noreply.github.com>
2025-10-12 10:45:13 +0900

    Update buzzer_init.c

--- app/src/buzzer_init.c
+++ app/src/buzzer_init.c
@@ -3,6 +3,9 @@
 
 static int buzzer_sys_init(const struct device *dev) {
     buzzer_init();
+    /* 전원 켜고 0.8초 뒤 테스트 톤 */
+    k_msleep(800);
+    buzzer_play_tone(2400, 55, 180);
     return 0;
 }
 
